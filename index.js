1860c03a8e646c83ae80edb0503dcd02aa706df5
William <mauritius.william.wijaya1@gmail.com>
2024-03-06 22:34:04 +0700

    adding homework and note

---
+++ index.js
@@ -0,0 +1,63 @@
+//? var, let dan const
+// let x // declare variable 
+
+// x = 2 // assign variable : INT
+
+// x = 3 // reassign variable 
+
+// x = 6 // reassign variable 
+
+// let y = 'asd' // : STRING
+
+// let z = true // : BOOLEAN
+
+//* SHORTCUT COMMENT : CTRL + /
+
+//? SWAP VARIABLE
+// let a = 5 // declare hanya 1x aja
+// let b = 2
+// let temp
+
+// temp = a
+// a = b
+// b = temp
+
+// console.log(a, b);
+
+//? IF ELSE
+let a = 5;
+
+// a === 5 // bandingkan valuenya
+// if (a === 5) {
+//     console.log("mantap");
+//     if (a < 3) {
+//         console.log('mantap bngt');
+//     } else {
+//         console.log('mantap aja ...');
+//     }
+// } else if (a > 3) {
+//     console.log("mantap x2");
+// } else if (a > 4) {
+//     console.log("mantap x3");
+// } else {
+//     console.log("tidak mantap");
+// }
+
+// switch (a) {
+//     case 3:
+//         console.log('hello world!');
+//         break;
+//     case 4:
+//         console.log('hello world!');
+//         break;
+//     case 5:
+//         console.log('hello world!');
+//         break;
+//     case 6:
+//         console.log('hello world!');
+//         break;
+
+//     default:
+//         console.log('hello aja');
+//         break;
+// }
